802ef046a365ff187ced2d2bf1e1a615b3bfe2a2
Nicolas Mesaritis <nmesaritis@ecommbx.com>
2025-09-20 19:31:32 +0300

    latest

--- GameBuilder/src/main.cpp
+++ GameBuilder/src/main.cpp
@@ -1,34 +1,54 @@
-#include "../../raylib/src/raylib.h"
-#include "../../rlImGui/imgui-master/imgui.h"
-#include "../../rlImGui/rlImGui.h"
+#include<raylib.h>
+#include<lwlog.h>   
+#include<nlohmann/json.hpp>
+#include<rlImGui.h>
+#include<imgui.h>
+
 
 int main()
 {
+	// Set up logging
+    auto logger = std::make_shared<lwlog::console_logger>("GB2D");
+    logger->set_pattern("[%T] [%n] [%l]: %v");
+    logger->info("GameBuilder2d starting up");
+
+
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "GameBuilder - raylib window");
-    
+   
+    SetTargetFPS(60);
+
+    rlImGuiSetup(true);
+
+    // Enable docking in ImGui (after context is created)
     ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
-    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
-        
+              
         rlImGuiBegin();
+
 		ImGui::ShowDemoWindow();
+
         rlImGuiEnd();
-        
+
         const char* msg = "Hello, raylib!";
+
         int fontSize = 20;
+
         int textWidth = MeasureText(msg, fontSize);
+
         DrawText(msg, (screenWidth - textWidth)/2, (screenHeight - fontSize)/2, fontSize, DARKGRAY);
 
         EndDrawing();
     }
 
     CloseWindow();
+
     return 0;
 }
