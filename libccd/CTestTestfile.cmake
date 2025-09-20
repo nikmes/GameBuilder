73963687ad884c754be8ff4820bde1da994bfbce
Nicolas Mesaritis <nmesaritis@ecommbx.com>
2025-09-20 21:47:16 +0300

    commit missed libccd

---
+++ libccd/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/nikme/source/repos/GameBuilder/libccd
+# Build directory: C:/Users/nikme/source/repos/GameBuilder/libccd
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
